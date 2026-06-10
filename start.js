c0a4958fd3d00635a569fb9e19be29c544db3275
xplendevgit <l.riveraa664@hotmail.com>
2026-06-10 11:46:17 -0400

    Agregar start.js de diagnóstico para capturar error real

--- start.js
+++ start.js
@@ -1,8 +1,33 @@
+import fs from 'fs';
 import { spawn } from 'child_process';
 
-// Hereda TODO el entorno automáticamente y arranca el servidor
-const child = spawn('node', ['dist/server/entry.mjs'], {
-  stdio: 'inherit'
+const log = (msg) => {
+  fs.appendFileSync('startup.log', msg + '\n');
+  console.log(msg);
+};
+
+log('Iniciando start.js de diagnóstico');
+log('PORT=' + process.env.PORT);
+log('HOST=' + process.env.HOST);
+log('PWD=' + process.cwd());
+
+const child = spawn('node', ['dist/server/entry.mjs'], { stdio: 'pipe' });
+
+child.stdout.on('data', (data) => {
+  process.stdout.write(data);
 });
 
-child.on('exit', (code) => process.exit(code));
+child.stderr.on('data', (data) => {
+  process.stderr.write(data);
+  fs.appendFileSync('startup.log', 'STDERR: ' + data);
+});
+
+child.on('error', (err) => {
+  log('Error al ejecutar: ' + err.message);
+  process.exit(1);
+});
+
+child.on('exit', (code) => {
+  log('Proceso hijo terminó con código ' + code);
+  process.exit(code);
+});
